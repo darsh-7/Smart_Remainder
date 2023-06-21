9ce18b32f87d11a45188f7f0fedadc8467af6e15
Darsh <ytgsasa@gmail.com>
2023-06-21 06:14:07 +0300

    add time button

--- app/src/main/java/com/example/smartremainder/SecondFragment.kt
+++ app/src/main/java/com/example/smartremainder/SecondFragment.kt
@@ -2,11 +2,14 @@ package com.example.smartremainder
 
 import android.app.DatePickerDialog
 import android.app.DatePickerDialog.OnDateSetListener
+import android.app.TimePickerDialog
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.DatePicker
+import android.widget.TimePicker
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.smartremainder.databinding.FragmentSecondBinding
@@ -18,7 +21,7 @@ import java.util.Locale
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment(), DatePickerDialog.OnDateSetListener {
+class SecondFragment : Fragment(), DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
     private var _binding: FragmentSecondBinding? = null
     private val calendar = Calendar.getInstance()
@@ -34,6 +37,7 @@ class SecondFragment : Fragment(), DatePickerDialog.OnDateSetListener {
 
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
         binding.date.text = getTodaysDate()
+/*
         val dateSetListener =
             OnDateSetListener { datePicker, year, month, day ->
                 var month = month
@@ -41,25 +45,63 @@ class SecondFragment : Fragment(), DatePickerDialog.OnDateSetListener {
                 val date: String = "${day}/${month}/${year}"
                 binding.date.setText(date)
                 calendar.set(year, month, day)
+                Log.i("Formatting", "${calendar.timeInMillis}")
+
             }
+*/
+/*
+        val OnTimeSetListener =
+            TimePickerDialog.OnTimeSetListener { timePicker, year, month, day ->
+                var month = month
+                month = month + 1
+                val date: String = "${day}/${month}/${year}"
+                binding.date.setText(date)
+                calendar.set(year, month, day)
+                Log.i("Formatting", "${calendar.timeInMillis}")
+
+            }
+*/
 
         binding.date.setOnClickListener(View.OnClickListener {
             DatePickerDialog(
                 container!!.context,
-                dateSetListener,
+                this,
                 calendar.get(Calendar.YEAR),
                 calendar.get(Calendar.MONTH),
                 calendar.get(Calendar.DAY_OF_MONTH)
             ).show()
         })
 
+        binding.time.setOnClickListener(View.OnClickListener {
+            TimePickerDialog(
+                container!!.context,
+                this,
+                calendar.get(Calendar.HOUR_OF_DAY),
+                calendar.get(Calendar.MINUTE),
+                false
+            ).show()
+        })
+
+
         return binding.root
 
     }
 
-    override fun onDateSet(p0: DatePicker?, p1: Int, p2: Int, p3: Int) {
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, day: Int) {
+        val month = month + 1
+        val date: String = "${day}/${month}/${year}"
+        binding.date.setText(date)
+        calendar.set(year, month, day)
+        Log.i("Formatting", "${calendar.timeInMillis}")
     }
-
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        calendar.apply {
+            set(Calendar.HOUR_OF_DAY, hourOfDay)
+            set(Calendar.MINUTE, minute)
+        }
+        val time: String = "${hourOfDay}:${minute}"
+        binding.time.setText(time)
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
